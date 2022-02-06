429280b777dae25099dee8b907eaf6ad1d5f4c96
草鞋没号 <308487730@qq.com>
2022-02-06 19:00:38 +0800

    feat: support statusbar

---
+++ src/StatusbarUi.ts
@@ -0,0 +1,30 @@
+import * as vscode from 'vscode';
+import Helper from './Helper';
+
+export default class StatusbarUi {
+  private static statusBarItem: vscode.StatusBarItem;
+
+  /**
+   * @see https://github.com/microsoft/vscode-extension-samples/blob/main/statusbar-sample/src/extension.ts
+   */
+  private static get statusbar() {
+    if (!this.statusBarItem) {
+      this.statusBarItem = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Right,
+        100,
+      );
+      this.statusBarItem.show();
+    }
+    return this.statusBarItem;
+  }
+
+  static init() {
+    this.statusbar.text = 'Vite$(zap)Vue';
+    this.statusbar.command = Helper.commands.TOGGLE_WEBVIEW;
+    this.statusbar.tooltip = 'Click to show dashboard';
+  }
+
+  static dispose() {
+    this.statusbar.dispose();
+  }
+}
